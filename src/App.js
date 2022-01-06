1766f6240a31ae809577fe33e693f248266b3e82
imaiyukari <imai.yukari.23@gmail.com>
2022-01-06 19:55:21 +0900

    station3

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ export const App = () => {
       <header>
         わんこの写真
       </header>
+      <p>可愛いわんちゃんの写真を表示するサイトです</p>
+      <img src='https://images.dog.ceo/breeds/akita/Akina_Inu_in_Riga_1.jpg'></img>
     </div>
   )
 }
