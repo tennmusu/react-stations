92749ac9b103231964d71ba2962fa5be34d8bd54
imaiyukari <imai.yukari.23@gmail.com>
2022-01-06 20:03:18 +0900

    station4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 // DO NOT DELETE
 
 import * as React from 'react'
+import { useState } from 'react';
+
 import './App.css'
 
 /**
@@ -8,13 +10,14 @@ import './App.css'
  * @type {React.FC}
  */
 export const App = () => {
+  const [dogUrl,setDogUrl] = useState('https://images.dog.ceo/breeds/akita/Akina_Inu_in_Riga_1.jpg');
   return (
     <div>
       <header>
         わんこの写真
       </header>
       <p>可愛いわんちゃんの写真を表示するサイトです</p>
-      <img src='https://images.dog.ceo/breeds/akita/Akina_Inu_in_Riga_1.jpg'></img>
+      <img src={dogUrl}></img>
     </div>
   )
 }
